36b173b44ed8eec16f4f9dd2dcd074e552572f0f
Shad Storhaug <shad@shadstorhaug.com>
2014-05-29 00:36:49 +0700

    Added solution demonstrating using AttributeRouting (MVC5) with security trimming

---
+++ MvcSiteMapProvider_316/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcSiteMapProvider_316.Startup))]
+namespace MvcSiteMapProvider_316
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
